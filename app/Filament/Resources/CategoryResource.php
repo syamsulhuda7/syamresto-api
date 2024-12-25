12c34a7ab1c1318f446c73b8b5e0c93f2a334f72
syamsulhuda7 <syamsulhuda748@gmail.com>
2024-12-25 14:40:13 +0700

    trial2

--- app/Filament/Resources/CategoryResource.php
+++ app/Filament/Resources/CategoryResource.php
@@ -26,10 +26,6 @@ class CategoryResource extends Resource
             ->schema([
                 Forms\Components\TextInput::make('name')
                 ->required(),
-                Forms\Components\TextInput::make('name')
-                ->required(),
-                Forms\Components\TextInput::make('name')
-                ->required(),
                 
                 Forms\Components\FileUpload::make('icon')
                 ->image()
