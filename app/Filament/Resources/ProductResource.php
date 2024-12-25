fb3f45d475421e0bc476420ad02f5ed9ad23dc3f
syamsulhuda7 <syamsulhuda748@gmail.com>
2024-12-25 14:04:17 +0700

    add resource

---
+++ app/Filament/Resources/ProductResource.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ProductResource\Pages;
+use App\Filament\Resources\ProductResource\RelationManagers;
+use App\Models\Product;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ProductResource extends Resource
+{
+    protected static ?string $model = Product::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\FileUpload::make('image_url')
+                ->image()
+                ->directory('product-images')
+                ->required()
+                ->columnSpan(2),
+
+                Forms\Components\TextInput::make('name')
+                ->required(),
+                Forms\Components\TextInput::make('price')
+                ->required()
+                ->numeric(),
+                Forms\Components\Textarea::make('description')
+                ->required()
+                ->columnSpan(2),
+                Forms\Components\Toggle::make('status')
+                ->required(),
+                // Forms\Components\TextInput::make('sold')
+                // ->required(),
+                Forms\Components\TextInput::make('category_id')
+                ->relationship('category', 'name')
+                ->required(),
+                
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListProducts::route('/'),
+            'create' => Pages\CreateProduct::route('/create'),
+            'edit' => Pages\EditProduct::route('/{record}/edit'),
+        ];
+    }
+}
--- config/cors.php
+++ config/cors.php
@@ -19,9 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [
-        // 'http://localhost:3000',
-         'apisyamstore.syamdev.my.id'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
